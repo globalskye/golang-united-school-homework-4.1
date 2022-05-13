b9b1db6052a393d6f6af98d7dc32ba3aad8797db
global <asgagagsad@gmail.com>
2022-05-13 15:26:37 -0400

    task fix

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/globalskye/golang-united-school-homework-4.1.git
 
 go 1.18
+
+require github.com/kyokomi/emoji v2.2.4+incompatible // indirect
--- reverse_string.go
+++ reverse_string.go
@@ -2,12 +2,18 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	//result := make([]string, len(input))
-	i := len(input)
-
+	if len(input) == 0 {
+		return ""
+	}
+	if len(input) == 1 {
+		return input
+	}
+	i := len(input) + 1
 	for {
 		if i != 1 {
+			//	i--
+			output = output + input[i-2:i-1]
 			i--
-			output = output + input[i-1:i]
 		} else {
 			break
 		}
