5a689f7db6e42f56aca7ac04080dd2a2bd6d8c9b
global <asgagagsad@gmail.com>
2022-05-13 14:57:56 -0400

    final

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,17 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	//result := make([]string, len(input))
+	i := len(input)
+
+	for {
+		if i != 1 {
+			i--
+			output = output + input[i-1:i]
+		} else {
+			break
+		}
+
+	}
 	return output
 }
