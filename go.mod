1e90c65521861cbc4c76ee9adb68478e1805765f
global <asgagagsad@gmail.com>
2022-05-17 21:54:47 -0400

    fix smile

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/globalskye/golang-united-school-homework-4.1.git
 
 go 1.18
 
-require github.com/kyokomi/emoji v2.2.4+incompatible // indirect
+require (
+	github.com/kyokomi/emoji v2.2.4+incompatible // indirect
+	github.com/kyokomi/emoji/v2 v2.2.9 // indirect
+)
--- reverse_string.go
+++ reverse_string.go
@@ -8,11 +8,12 @@ func ReverseString(input string) (output string) {
 	if len(input) == 1 {
 		return input
 	}
+	result := []rune(input)
 	i := len(input) + 1
 	for {
 		if i != 1 {
 			//	i--
-			output = output + input[i-2:i-1]
+			output += string(result[i-2 : i-1])
 			i--
 		} else {
 			break
